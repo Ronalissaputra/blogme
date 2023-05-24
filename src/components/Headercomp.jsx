0dc0a1285e58fe0f2859c5b9d9318abdc4d7887c
Ronalis Saputra <macbookair@192.168.1.13>
2023-05-25 01:09:02 +0800

    capek

--- src/components/Headercomp.jsx
+++ src/components/Headercomp.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
-import { AiOutlineCloseCircle } from "react-icons/AI";
-import { HiOutlineMenuAlt4 } from "react-icons/HI";
+import { AiOutlineCloseCircle } from "react-icons/Ai";
+import { HiOutlineMenuAlt4 } from "react-icons/Hi";
 
 const Headercomp = () => {
   const [open, setOpen] = useState(false);
--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,12 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ["react-icons/Bs", "react-icons/Si"],
+      external: [
+        "react-icons/Bs",
+        "react-icons/Si",
+        "react-icons/Hi",
+        "react-icons/Ai",
+      ],
     },
   },
 });
